a09706eb6f2838f90a848419e9aae44cf7a91dfe
JuanmaRuizF <69359816+JuanmaRuizF@users.noreply.github.com>
2020-08-31 01:33:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeBg() { 
+    var cuerpoweb = document.body; 
+    cuerpoweb.classList.toggle("oscuro"); 
+}
+
+
+$(function(){
+            $("[data-toggle='tooltip' ]").tooltip();
+});
